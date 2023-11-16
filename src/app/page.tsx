13f3b37c1e565d4e3003cbc7ceed99d6a4c0ca25
Meta <petmetic@gmail.com>
2023-11-16 10:41:29 +0100

    making a list of plants from hard-coded dict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
-import {Button} from "@/components/ui/button";
-
+import {PlantList} from "@/components/plant-list";
 
 export default function Home() {
-  return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <p>Hello world! This is Meta...</p>
-          <Button>Sigh IN</Button>
-      </main>
-  )
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <p>Hello world! This is Meta...</p>
+
+            <PlantList>
+            </PlantList>
+
+
+        </main>
+    )
 }
 
 
