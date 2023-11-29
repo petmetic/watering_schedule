f8c3dadfaa34a03051dc947887b4c8875357d5f2
Jure Cuhalev <jure@cuhalev.com>
2023-11-29 16:36:02 +0100

    Use jsdom to run tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,5 +2,7 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
-  test: {},
+  test: {
+    environment: 'jsdom',
+  },
 });
