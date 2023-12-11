d43eda55881c1437d55a9c4b6a41acb937b1b022
Meta <petmetic@gmail.com>
2023-12-11 11:04:08 +0100

    create plant page fix, added skeletons for dashboard

--- src/app/ui/skeletons.tsx
+++ src/app/ui/skeletons.tsx
@@ -9,3 +9,17 @@
 //     </div>
 //   );
 // }
+import PlantListSkeleton from "./plant-list-skeleton";
+
+const shimmer =
+  "before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent";
+
+export default function DashboardSkeleton() {
+  return (
+    <>
+      <div>
+        <PlantListSkeleton />
+      </div>
+    </>
+  );
+}
