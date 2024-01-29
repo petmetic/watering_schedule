6f714b9707aece87e3a07cc41b0407c7e066d521
Meta <petmetic@gmail.com>
2024-01-29 19:23:54 +0100

    rebase 5

--- src/app/api/plants/route.ts
+++ src/app/api/plants/route.ts
@@ -1,4 +1,6 @@
-export async function GET(request: Request) {}
+export async function GET(request: Request, res: Response) {
+  console.log("GET REQUEST");
+}
 export async function HEAD(request: Request) {}
 
 export async function POST(request: Request) {}
