82224339d546aa97350c64e1069cdc30836d826a
Meta <petmetic@gmail.com>
2024-01-29 19:37:00 +0100

    rebase fix

--- src/app/dashboard/plants/add/page.tsx
+++ src/app/dashboard/plants/add/page.tsx
@@ -1,15 +1,6 @@
 import { Metadata } from "next";
-<<<<<<< HEAD
-// import Breadcrumbs from "@/app/ui/plants/breadcrumbs";
-import { PlantForm } from "@/app/ui/plants/add-plant";
-=======
 import Breadcrumbs from "@/app/ui/plants/breadcrumbs";
-<<<<<<< HEAD:src/app/dashboard/plants/create/page.tsx
-import { PlantForm } from "@/app/ui/plants/create-plant";
->>>>>>> 19f3889 (rebase #5)
-=======
 import { PlantForm } from "@/app/ui/plants/add-plant";
->>>>>>> f5b3fb5 (add plant form partial):src/app/dashboard/plants/add/page.tsx
 
 export const metadata: Metadata = {
   title: "Create Plant",
