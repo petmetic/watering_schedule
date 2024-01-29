7e572b5d95efebf448f911cabcae720c5b17075f
Meta <petmetic@gmail.com>
2024-01-29 17:53:17 +0100

    reverting to previous commit

--- src/app/ui/plants/add-plant.tsx
+++ src/app/ui/plants/add-plant.tsx
@@ -42,37 +42,37 @@ import { Metadata } from "next";
 //   title: "Add plant",
 // };
 const FormSchema = z.object({
-  plantId: z.number(),
-  plantName: z.string().min(2, {
+  id: z.number(),
+  name: z.string().min(2, {
     message: "Plant name must be at least 2 characters.",
   }),
-  plantLocation: z.string().min(2, {
+  location: z.string().min(2, {
     message: "Plant location must be at least 2 characters.",
   }),
-  frequency: z.coerce.number().min(1, {
+  frequency: z.number().min(1, {
     message: "Watering time should be an integer, specified in days.",
   }),
-  waterVolume: z.string().min(1, {
+  volume: z.string().min(1, {
     message: "",
   }),
-  plantInstructions: z.string().min(1, {
+  instructions: z.string().min(1, {
     message: "Please provide instructions for taking care of the plant.",
   }),
-  plantStatus: z.string(),
-  startWateringDate: z.string(),
-  endWateringDate: z.string(),
+  status: z.string(),
+  start: z.string(),
+  end: z.string(),
 });
 
 export function PlantForm() {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
-      plantName: "",
-      plantLocation: "",
-      waterVolume: "",
-      plantInstructions: "",
-      startWateringDate: "",
-      endWateringDate: "",
+      name: "",
+      location: "",
+      volume: "",
+      instructions: "",
+      start: "",
+      end: "",
     },
   });
   const waterVolume = [
@@ -98,192 +98,192 @@ export function PlantForm() {
   }
 
   return (
-    <Form
-      {...form}
-      onSubmit={form.handleSubmit(onSubmit)}
-      className="w-2/3 space-y-6"
-    >
-      <FormField
-        control={form.control}
-        name="plantName"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Plant Name</FormLabel>
-            <FormControl>
-              <Input placeholder="name of plant" {...field} />
-            </FormControl>
-            <FormDescription>Insert name of plant.</FormDescription>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-      <FormField
-        control={form.control}
-        name="plantLocation"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Location</FormLabel>
-            <FormControl>
-              <Input placeholder="bookshelves" {...field} />
-            </FormControl>
-            <FormDescription>Insert location of plant.</FormDescription>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-      <FormField
-        control={form.control}
-        name="frequency"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Watering frequency</FormLabel>
-            <FormControl>
-              <Input placeholder="3" {...field} />
-            </FormControl>
-            <FormDescription>Insert time in days for watering.</FormDescription>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-      <FormField
-        control={form.control}
-        name="waterVolume"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Volume of water</FormLabel>
-            <Select onValueChange={field.onChange} defaultValue={field.value}>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Plant Name</FormLabel>
               <FormControl>
-                <SelectTrigger>
-                  <SelectValue placeholder="100 ml" />
-                </SelectTrigger>
+                <Input placeholder="name of plant" {...field} />
               </FormControl>
-              <SelectContent>
-                {waterVolume.map((waterVolume, index) => (
-                  <SelectItem key={index} value={waterVolume.value}>
-                    {waterVolume.name}
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-            <FormDescription>Select amount of water.</FormDescription>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-      <FormField
-        control={form.control}
-        name="plantInstructions"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Instructions</FormLabel>
-            <FormControl>
-              <Textarea placeholder="Water me with a mist." {...field} />
-            </FormControl>
-            <FormDescription>
-              Insert instructions for taking care of plant.
-            </FormDescription>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-      <FormField
-        control={form.control}
-        name="startWateringDate"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Start watering date</FormLabel>
-            <Popover>
-              <PopoverTrigger asChild>
+              <FormDescription>Insert name of plant.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="location"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Location</FormLabel>
+              <FormControl>
+                <Input placeholder="bookshelves" {...field} />
+              </FormControl>
+              <FormDescription>Insert location of plant.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="frequency"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Watering frequency</FormLabel>
+              <FormControl>
+                <Input placeholder="3" {...field} />
+              </FormControl>
+              <FormDescription>
+                Insert time in days for watering.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="volume"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Volume of water</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
                 <FormControl>
-                  <Button
-                    variant={"outline"}
-                    className={cn(
-                      "w-[240px] pl-3 text-left font-normal",
-                      !field.value && "text-muted-foreground",
-                    )}
-                  >
-                    {field.value ? (
-                      format(field.value, "PPP")
-                    ) : (
-                      <span>Pick a date</span>
-                    )}
-                    <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                  </Button>
+                  <SelectTrigger>
+                    <SelectValue placeholder="100 ml" />
+                  </SelectTrigger>
                 </FormControl>
-              </PopoverTrigger>
-              <PopoverContent className="w-auto p-0" align="start">
-                <Calendar
-                  mode="single"
-                  selected={field.value}
-                  onSelect={field.onChange}
-                  disabled={(date) =>
-                    date > new Date() || date < new Date("1900-01-01")
-                  }
-                  initialFocus
-                />
-              </PopoverContent>
-            </Popover>
-            <FormDescription>
-              Insert date of beginning of watering.
-            </FormDescription>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-      <FormField
-        control={form.control}
-        name="endWateringDate"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>End watering date</FormLabel>
-            <Popover>
-              <PopoverTrigger asChild>
-                <FormControl>
-                  <Button
-                    variant={"outline"}
-                    className={cn(
-                      "w-[240px] pl-3 text-left font-normal",
-                      !field.value && "text-muted-foreground",
-                    )}
-                  >
-                    {field.value ? (
-                      format(field.value, "PPP")
-                    ) : (
-                      <span>Pick a date</span>
-                    )}
-                    <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                  </Button>
-                </FormControl>
-              </PopoverTrigger>
-              <PopoverContent className="w-auto p-0" align="start">
-                <Calendar
-                  mode="single"
-                  selected={field.value}
-                  onSelect={field.onChange}
-                  disabled={(date) =>
-                    date > new Date() || date < new Date("1900-01-01")
-                  }
-                  initialFocus
-                />
-              </PopoverContent>
-            </Popover>
-            <FormDescription>Insert date of end of watering.</FormDescription>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-      <FormField
-        name="plantImage"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Plant Image</FormLabel>
-            <Input id="image" type="file" />
-            <FormDescription>Choose plant image.</FormDescription>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-      <Button type="submit">Submit</Button>
+                <SelectContent>
+                  {waterVolume.map((waterVolume, index) => (
+                    <SelectItem key={index} value={waterVolume.value}>
+                      {waterVolume.name}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <FormDescription>Select amount of water.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="instructions"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Instructions</FormLabel>
+              <FormControl>
+                <Textarea placeholder="Water me with a mist." {...field} />
+              </FormControl>
+              <FormDescription>
+                Insert instructions for taking care of plant.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="start"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Start watering date</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-[240px] pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground",
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    disabled={(date) =>
+                      date > new Date() || date < new Date("1900-01-01")
+                    }
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormDescription>
+                Insert date of beginning of watering.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="end"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>End watering date</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-[240px] pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground",
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    disabled={(date) =>
+                      date > new Date() || date < new Date("1900-01-01")
+                    }
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormDescription>Insert date of end of watering.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          name="photo"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Plant Image</FormLabel>
+              <Input id="image" type="file" />
+              <FormDescription>Choose plant image.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
     </Form>
   );
 }
