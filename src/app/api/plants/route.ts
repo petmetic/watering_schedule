17db48e9c143d187f0f6e68d01b3474877a9aa25
Meta <petmetic@gmail.com>
2024-02-15 11:06:01 +0100

    refactored post request for compartmentalisation

--- src/app/api/plants/route.ts
+++ src/app/api/plants/route.ts
@@ -23,25 +23,7 @@ export async function GET(request: Request) {
   return data.results;
 }
 
-export async function POST(form: HTMLFormElement) {
-  const photoField = document.getElementById("photo") as HTMLInputElement;
-  const file = photoField?.files ? photoField.files[0] : null;
-
-  let formData = new FormData();
-
-  for (const [key, value] of Object.entries(form)) {
-    formData.append(key, value);
-    if (key == "photo") {
-      formData.set("photo", file, file.name);
-    } else if (key == "start") {
-      let start = form.start.toISOString();
-      formData.set("start", start);
-    } else if (key == "end") {
-      let end = form.start.toISOString();
-      formData.set("end", end);
-    }
-  }
-
+export async function POST(formData) {
   const res = await fetch("http://127.0.0.1:8000/plants/", {
     method: "POST",
     headers: {},
