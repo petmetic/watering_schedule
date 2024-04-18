d442adcacf89863f0ae72e8929d46f2cc0a1b301
Meta <petmetic@gmail.com>
2024-04-18 12:01:59 +0200

    added collapsible component + set watered state in plant-list to true

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -1,8 +1,10 @@
-import { Leaf } from "lucide-react";
-import { PlusCircle } from "lucide-react";
-import { CircleSlash } from "lucide-react";
-import { Frown } from "lucide-react";
-import { ChevronDown } from "lucide-react";
+import {
+  ChevronDown,
+  CircleSlash,
+  Frown,
+  Leaf,
+  PlusCircle,
+} from "lucide-react";
 
 const Logo = () => {
   return <Leaf className="h-20 w-20" />;
